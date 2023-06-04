ff5f3fa0bedc29c1f9436f4eb53d49fc8722d286
Madina <masrorkhojaeva@gmail.com>
2023-06-05 00:53:16 +0500

    New commit

--- dist/new.js
+++ dist/new.js
@@ -1,3 +1,1 @@
 "use strict";
-const addMovieForm = document.querySelector(".add-movie-form");
-const genreSelect = document.querySelector(".genre-select");
--- src/new.ts
+++
@@ -1,3 +0,0 @@
-
-const addMovieForm = document.querySelector<HTMLFormElement>(".add-movie-form")!;
-const genreSelect = document.querySelector<HTMLSelectElement>(".genre-select")!;
